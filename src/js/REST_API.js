a7e70cd4f3d5bdd1ccbed5cc55143717119b9084
tjrdud123 <youmr100@naver.com>
2021-02-01 20:04:34 +0900

    fix: fetch 에러 핸들링 추가 try catch 코드 추가

--- src/js/REST_API.js
+++ src/js/REST_API.js
@@ -6,36 +6,58 @@ export async function getData(type, query) {
     url += "?";
     for (let key in query) url += `${key}=${query[key]}&`;
   }
-  const res = await fetch(url);
-  const resData = await res.json();
-  return resData;
+  try {
+    const res = await fetch(url);
+    const resData = await res.json();
+    if(res.status !== 200) throw new Error("error");
+    
+    return resData;
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 export async function deleteData(type, id) {
   let url = `${URL}/${type}/${id}`;
-  const res = await fetch(url, {
-    method: "DELETE"
-  });
+  try {
+    const res = await fetch(url, {
+      method: "DELETE"
+    });
+    if(res.status !== 200) throw new Error("error");
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 export async function postData(type, data) {
   let url = `${URL}/${type}`;
-  const res = await fetch(url, {
-    method: 'post',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data)
-  });
+  try {
+    const res = await fetch(url, {
+      method: 'post',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
+    if(res.status !== 201) throw new Error("error");
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 export async function putData(type, data) {
   let url = `${URL}/${type}/${data.id}`;
-  fetch(url, {
-    method: 'PUT',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data)
-  })
+  try {
+    const res = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
+    if(res.status !== 200) throw new Error("error");
+
+  } catch(err) {
+    console.error(err);
+  }
 }
