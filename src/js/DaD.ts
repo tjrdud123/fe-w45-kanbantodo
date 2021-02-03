0ef906cbb242ba55eb4a35ffc9a490c53c2ffa61
tjrdud123 <youmr100@naver.com>
2021-02-03 18:15:12 +0900

    Modal 구현 마무리, DaD ts로 구현 시작

--- src/js/DaD.ts
+++ src/js/DaD.ts
@@ -1,8 +1,8 @@
-/*
-  Drag and Drop
-*/
+interface DragAndDrop {
+  root: HTMLElement;
+}
 
-export default class DaD {
+export default class DaD implements DragAndDrop {
   public root: HTMLElement;
   constructor() {
     this.root = document.body;
